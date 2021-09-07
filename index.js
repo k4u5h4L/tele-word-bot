9031dadef39d5d0de815a8d6e71519de49e77008
k4u5h4L <kaushal.v.bhat@gmail.com>
2021-09-07 20:59:47 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+//const results = await prisma.post.findMany({
+//     skip: 3,
+//     take: 4,
+//   })
+
+require("dotenv").config();
+
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, resp);
+});
+
+bot.on("message", (msg) => {
+    const chatId = msg.chat.id;
+
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendMessage(chatId, "Received your message");
+});
